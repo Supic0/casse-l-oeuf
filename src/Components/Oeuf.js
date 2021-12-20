a850561400428b31211cabf63d90d2e7c66847ac
Supico Bruno <supico.bruno@gmail.com>
2021-12-20 22:50:02 +0100

    modify src of img to simple string for ios14

--- src/Components/Oeuf.js
+++ src/Components/Oeuf.js
@@ -1,12 +1,6 @@
 import React, { useState} from 'react'
 import './Oeuf.css'
 import Activity from './Activity'
-import egg1 from '../images/egg1.png'
-import egg2 from '../images/egg2.png'
-import egg3 from '../images/egg3.png'
-import egg41 from '../images/egg4-1.png'
-import egg42front from '../images/egg42front.png'
-import egg42back from '../images/egg42back.png'
 import panier from '../images/panier.png'
 
 export default function Oeuf({listActivities, setAllLoaded}) {
@@ -31,23 +25,23 @@ export default function Oeuf({listActivities, setAllLoaded}) {
                     </div>}
             <div className="Oeufanim">
 
-                {(nbClick <= nbClickMax) && <img alt="Oeuf cassé arrière" src={egg42back} className={`Oeuf ${(nbClick >= nbClickMax) ? "down": ""}`}></img>}
-                {(listActivities?.length!==0)&&<Activity nbClickMax={nbClickMax}setAllLoaded={setAllLoaded} listActivities={listActivities} setNbClick={setNbClick} nbClick={nbClick}/>}
-                {(nbClick <= nbClickMax) && <img alt="Oeuf cassé Front up" src={egg41} className={`Oeuf ${(nbClick >= nbClickMax) ? "up":""}`} ></img>}
-                {(nbClick <= nbClickMax) && <img alt="Oeuf cassé Front down" src={egg42front} className={`Oeuf ${(nbClick >= nbClickMax) ? "down":""}`}></img>}
+                {(nbClick <= nbClickMax) && <img alt="Oeuf cassé arrière" src="./images/egg42back.png" className={`Oeuf ${(nbClick >= nbClickMax) ? "down": ""}`}></img>}
+                {(listActivities?.length!==0)&&<Activity nbClickMax={nbClickMax} setAllLoaded={setAllLoaded} listActivities={listActivities} setNbClick={setNbClick} nbClick={nbClick}/>}
+                {(nbClick <= nbClickMax) && <img alt="Oeuf cassé Front up" src="./images/egg4-1.png" className={`Oeuf ${(nbClick >= nbClickMax) ? "up":""}`} ></img>}
+                {(nbClick <= nbClickMax) && <img alt="Oeuf cassé Front down" src="./images/egg42front.png" className={`Oeuf ${(nbClick >= nbClickMax) ? "down":""}`}></img>}
 
             </div>
 
-            {(nbClick < nbClickMax) && <img alt="Oeuf cassé" src={egg3} className="Oeuf" onClick={tapeOeuf}>
+            {(nbClick < nbClickMax) && <img alt="Oeuf cassé" src="./images/egg3.png" className="Oeuf" onClick={tapeOeuf}>
             </img>}
 
-            {(nbClick < nbClickMax*50/100) && <img alt="Oeuf cassé" src={egg2} className="Oeuf" onClick={tapeOeuf}>
+            {(nbClick < nbClickMax*50/100) && <img alt="" src="./images/egg2.png" className="Oeuf" onClick={tapeOeuf}>
             </img>}
 
-            {(nbClick < nbClickMax*5/100) && <img alt="Oeuf cassé" src={egg1} className="Oeuf" onClick={tapeOeuf} >
+            {(nbClick < nbClickMax*5/100) && <img src="./images/egg1.png" className="Oeuf" onClick={tapeOeuf} >
             </img>}
 
-            {(nbClick < nbClickMax) && <img src={panier} alt="panier" git className="panier" onLoad={() => setAllLoaded(true)}></img>}
+            {(nbClick < nbClickMax) && <img src={panier} alt="panier" className="panier" onLoad={() => setAllLoaded(true)}></img>}
 
             {(nbClick < 1) &&<lottie-player src="https://assets4.lottiefiles.com/packages/lf20_RxwlFx.json" background="transparent" speed="1" id="tapIcon" loop  autoplay></lottie-player>}
         </div>
